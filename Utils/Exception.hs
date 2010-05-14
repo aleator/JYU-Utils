c536561f55f927a64abcbf5e4233fb9c7db1d0f0
Ville Tirronen <ville.e.t.tirronen@jyu.fi>
2010-05-14 10:35:57 +0300

    Added Utils.Exception module

---
+++ Utils/Exception.hs
@@ -0,0 +1,16 @@
+module Utils.Exception where
+import Control.Exception as E
+import System.Exit
+
+tagE op msg = mapException tag op
+    where 
+        tag :: SomeException -> ErrorCall
+        tag e = ErrorCall $ "Error in "++msg++" ("++show e++")"
+
+onErrorTerminateWith op msg = op 
+                               `E.catch` 
+                               (\e -> putStrLn ("Error on "++msg
+                                                           ++" ("
+                                                           ++show (e::SomeException)
+                                                           ++")") 
+                                      >> exitWith (ExitFailure 1)) 
